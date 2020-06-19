dda841ba477fd0d54150f4bf4456a329c107b7e0
jay.sueno <jay.sueno@gmail.com>
2020-06-19 16:16:42 -0700

    2nd pass at PyPoll homework. Not using pandas and useing for loop while using a conditional if/then within a list. I've figured out how to append name list. Next steps are assigning vote counts for each candidate and creating function to calculate percentages.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,36 +7,46 @@ poll_csv = os.path.join('Resources', 'election_data.csv')
 poll_output_file = os.path.join('Analysis', 'PyPoll_output_file.txt')
 print(poll_csv)
 
-poll_df = pd.read_csv(poll_csv)
-poll_df.head(5)
+# poll_df = pd.read_csv(poll_csv)
+# poll_df.head(5)
 
-candidates = poll_df["Candidate"].value_counts()
-print(candidates)
+# candidates = poll_df["Candidate"].value_counts()
+# print(candidates)
 
 
 # define variables
-# total_votes = 0
+total_votes = 0
+candidates = []
 # total_profit_loss = 0
 # greatest_inc = 0
 # greatest_dec = 0
 
 # * You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
-# with open(poll_csv, 'r') as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=",")
-#     header = next(csvreader)
-#     print(csvreader)
+with open(poll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    print(csvreader)
 
     #start the for loop
-    # for row in csvreader:
+    for row in csvreader:
 
 #   * The total number of votes cast
-        # total_votes += 1
+        total_votes += 1
 # print (total_votes)
 
 #   * A complete list of candidates who received votes
+# using an if then conditional to run down the list, if name isn't in the list, then append to list. if name is on the list, then skip.
+        # if row[2] != candidates:
+        #     candidates.append(row[2])
+
+        if row[2] not in candidates: 
+            candidates.append(row[2])
+            #manipulate the candidates list outside of the for loop
+print(candidates)
         
 #   * The percentage of votes each candidate won
-
+# track all the votes associated with each candidate using a list or a dictionary
+#clue - look at a counter maybe? in or not in a list. 
 #   * The total number of votes each candidate won
 
 #   * The winner of the election based on popular vote.
