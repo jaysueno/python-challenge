dd989f3a9b806ff46b5272ecec2bf5445aa446b6
jay.sueno <jay.sueno@gmail.com>
2020-06-20 22:34:56 -0700

    all calculations are done. i used lists, if then statments, for loops.

--- PyPoll/main_pypoll.py
+++ PyPoll/main_pypoll.py
@@ -7,20 +7,11 @@ poll_csv = os.path.join('Resources', 'election_data.csv')
 poll_output_file = os.path.join('Analysis', 'PyPoll_output_file.txt')
 print(poll_csv)
 
-# poll_df = pd.read_csv(poll_csv)
-# poll_df.head(5)
-
-# candidates = poll_df["Candidate"].value_counts()
-# print(candidates)
-
-
 # define variables
 total_votes = 0
 candidates = []
-votes = [0, 0, 0, 0]
-# total_profit_loss = 0
-# greatest_inc = 0
-# greatest_dec = 0
+votes = [int(0), int(0), int(0), int(0)]
+vote_percentages =[float(0), float(0), float(0), float(0)]
 
 # * You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
 with open(poll_csv, 'r') as csvfile:
@@ -36,26 +27,51 @@ with open(poll_csv, 'r') as csvfile:
 
 
 #   * A complete list of candidates who received votes
-# using an if then conditional to run down the list, if name isn't in the list, then append to list. if name is on the list, then skip.
-        # if row[2] != candidates:
-        #     candidates.append(row[2])
-
         if row[2] not in candidates: 
             candidates.append(row[2])
-            #manipulate the candidates list outside of the for loop
+        #     # ['Khan', 'Correy', 'Li', "O'Tooley"]
+        #     #manipulate the candidates list outside of the for loop
+        # break
+# print(candidates[1])
+csvfile.close()
 
+with open(poll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    print(csvreader)
+# for row in csvreader:
+    for row in csvreader:
 
-print(candidates)
+    # for row in csvreader:
 
-print (total_votes)        
+        if row[2] == candidates[0]:
+            votes[0] = votes[0] + 1
+        elif row[2] == candidates[1]:
+            votes[1] = votes[1] + 1
+        elif row[2] == candidates[2]:
+            votes[2] = votes[2] + 1
+        elif row[2] == candidates[3]:
+            votes[3] = votes[3] + 1
+
+#Calculations
 #   * The percentage of votes each candidate won
+vote_percentages[0] = candidate0_percentage = votes[0] / total_votes
+vote_percentages[1] = candidate1_percentage = votes[1] / total_votes
+vote_percentages[2] = candidate2_percentage = votes[2] / total_votes
+vote_percentages[3] = candidate3_percentage = votes[3] / total_votes
+print (vote_percentages)
+print(candidates)
+  
+# print (votes)     
+
 # track all the votes associated with each candidate using a list or a dictionary
         
 #clue - look at a counter maybe? in or not in a list. if row[2] == candidate: do a count
 #   * The total number of votes each candidate won
-
+print (total_votes) 
 #   * The winner of the election based on popular vote.
-
+winner_int = max(vote_percentages)
+print(candidates[int(winner_int)])
 # * As an example, your analysis should look similar to the one below:
 
 #   ```text
