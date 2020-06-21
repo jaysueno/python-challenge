266b0215797e7a39ddd7724f15fe4356a7c806d3
jay.sueno <jay.sueno@gmail.com>
2020-06-20 23:37:45 -0700

    PyPoll finished assignment!

--- PyPoll/main_pypoll.py
+++ PyPoll/main_pypoll.py
@@ -1,6 +1,5 @@
 import os
 import csv
-import pandas as pd
 
 # define file path
 poll_csv = os.path.join('Resources', 'election_data.csv')
@@ -9,41 +8,31 @@ print(poll_csv)
 
 # define variables
 total_votes = 0
-candidates = []
-votes = [int(0), int(0), int(0), int(0)]
-vote_percentages =[float(0), float(0), float(0), float(0)]
+candidates = [] # empty list that we will ammend with candidates
+votes = [int(0), int(0), int(0), int(0)] # list of place holders for candidate votes
+vote_percentages =[float(0), float(0), float(0), float(0)] # list placeholders for candidate percentages
 
-# * You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+# Open data file with a csv reader and skip the header
 with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
     print(csvreader)
-
     #start the for loop
     for row in csvreader:
-
-#   * The total number of votes cast
+        # Calculate total votes with a counter
         total_votes += 1
-
-
-#   * A complete list of candidates who received votes
+        # use a conditional statement to find unique candidate names to append into the candidates list.
         if row[2] not in candidates: 
             candidates.append(row[2])
-        #     # ['Khan', 'Correy', 'Li', "O'Tooley"]
-        #     #manipulate the candidates list outside of the for loop
-        # break
-# print(candidates[1])
 csvfile.close()
 
 with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
     print(csvreader)
-# for row in csvreader:
+    # Start another for loop.
     for row in csvreader:
-
-    # for row in csvreader:
-
+        # Vote counter for each candidate. If the index name matches the row, then add it to the corresponding votes list index.
         if row[2] == candidates[0]:
             votes[0] = votes[0] + 1
         elif row[2] == candidates[1]:
@@ -53,64 +42,29 @@ with open(poll_csv, 'r') as csvfile:
         elif row[2] == candidates[3]:
             votes[3] = votes[3] + 1
 
-#Calculations
-#   * The percentage of votes each candidate won
-vote_percentages[0] = candidate0_percentage = votes[0] / total_votes
-vote_percentages[1] = candidate1_percentage = votes[1] / total_votes
-vote_percentages[2] = candidate2_percentage = votes[2] / total_votes
-vote_percentages[3] = candidate3_percentage = votes[3] / total_votes
-print (vote_percentages)
-print(candidates)
-  
-# print (votes)     
+# Calculations - Vote percentages of each candidate. Assign the value to an index of vote percentages. Format the votes
+vote_percentages[0] = candidate0_percentage = {'{:.3f}%'.format((votes[0] / total_votes) * 100)}
+vote_percentages[1] = candidate1_percentage = {'{:.3f}%'.format((votes[1] / total_votes) * 100)}
+vote_percentages[2] = candidate2_percentage = {'{:.3f}%'.format((votes[2] / total_votes) * 100)}
+vote_percentages[3] = candidate3_percentage = {'{:.3f}%'.format((votes[3] / total_votes) * 100)}
 
-# track all the votes associated with each candidate using a list or a dictionary
-        
-#clue - look at a counter maybe? in or not in a list. if row[2] == candidate: do a count
-#   * The total number of votes each candidate won
-print (total_votes) 
-#   * The winner of the election based on popular vote.
-winner_int = max(vote_percentages)
-print(candidates[int(winner_int)])
-# * As an example, your analysis should look similar to the one below:
+# Find the winner by finding the max() within the votes list and then pulling the same index from the candidates list.
+winner_int = votes.index(max(votes))
+winner = candidates[winner_int]
 
-#   ```text
-#   Election Results
-#   -------------------------
-#   Total Votes: 3521001
-#   -------------------------
-#   Khan: 63.000% (2218231)
-#   Correy: 20.000% (704200)
-#   Li: 14.000% (492940)
-#   O'Tooley: 3.000% (105630)
-#   -------------------------
-#   Winner: Khan
-#   -------------------------
-#   ```
-# * In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
-# Jay Sueno  5:09 PM
-# hello! yes. i'm on the part where i can pull the names of the candidates. now i need to figure out how to create a list or dictionary of some sort to assign number of votes. how far have you gotten?
-
-
-
-
-
-# 5:09
-# i decided not to use pandas in this exercise so it's a little bit more challenging for me.
-
-# Minerva Banuelos  6:24 PM
-# I submitted it last night. I think Thompson asked about using pandas...but even so we use pandas for the next homework.
-# Referring to exercises, "02-Ins_Dicts" and "06-Evr_List_Comprehensions" from the 3rd session helps with the basics. Let me see which other example helped me with that part...hang on!
-# :heart:
-# 1
-
-# 6:25
-# Sorry, I just got home and saw your messages.
-
-# Minerva Banuelos  6:52 PM
-# For dictionary, a good reference: "03-Stu_HobbyBook"
-# For conditional statements: "07-In_Conditionals" + "08-Stu_ConditionalConundrum"
-# :heart:
-# 1
-
+# Print results in terminal and onto PyPoll_output_file.txt file in the analysis folder
+with open(poll_output_file, 'w') as txt_file:
+    output_string = ("Election Results \n"
+                    "-------------------------\n"
+                    f"Total Votes: {total_votes}\n"
+                    "-------------------------\n"
+                    f"{candidates[0]}: {candidate0_percentage} ({votes[0]})\n"
+                    f"{candidates[1]}: {candidate1_percentage} ({votes[1]})\n" 
+                    f"{candidates[2]}: {candidate2_percentage} ({votes[2]})\n" 
+                    f"{candidates[3]}: {candidate3_percentage} ({votes[3]})\n" 
+                    "-------------------------\n"
+                    f"Winner: {winner}\n"
+                    "-------------------------\n"
+                    )
+    txt_file.write(output_string)
+print(output_string)
